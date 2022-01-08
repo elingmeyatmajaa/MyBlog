554b28324a43b085ab5411be1baee804deede3f2
Eling Meyatmaja <eling.atma@gmail.com>
2022-01-09 00:17:00 +0700

    master layout and single post

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+
+Route::get('/post', function () {
+    return view('post');
+})->name('post');
 
 
 
