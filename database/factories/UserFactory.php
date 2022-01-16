d9ca60df61696e667a34fed2f5d0b0cff1e243e8
Eling Meyatmaja <eling.atma@gmail.com>
2022-01-16 14:46:02 +0700

    user and role factories enable and disbale foreindkeyconstraint

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,16 +2,34 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\Role;
 
 class UserFactory extends Factory
 {
+    /** 
+     * 
+     * The name of the factory's correspoinding model.
+     * 
+     * @var string
+     * 
+     */
+
+
+
+    protected $model = User::class;
+
+
     /**
      * Define the model's default state.
      *
      * @return array
      */
+
+
+
     public function definition()
     {
         return [
@@ -20,6 +38,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'role_id' => Role::where('name', 'user')->first()->id
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Disbale foreign key constraints for users and enable it again.
+        Schema::disableForeignKeyConstraints();
+        \App\Models\Role::truncate();
+        \App\Models\User::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        // Create roles and users
+        \App\Models\Role::factory(1)->create();
+        \App\Models\User::factory(10)->create();
     }
 }
