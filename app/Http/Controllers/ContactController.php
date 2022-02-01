357cb627d1a4c2260346a3941d352cc46121f061
Eling Meyatmaja <eling.atma@gmail.com>
2022-02-01 08:30:16 +0700

    Working Contact Controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\Test;
 use Illuminate\Http\Request;
 use App\Models\Contact;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -23,6 +25,8 @@ class ContactController extends Controller
             "subject" => 'nullable|min:5|max:50',
             "message" => 'required|min:5|max:500',
         ]));
+
+        Mail::to("eling.atma@gmail.com")->send(new Test("eling"));
 
         return redirect()->route('contact.create')->with('success', 'Your Message has been sent');
     }
