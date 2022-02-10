2f71b9dfcd0fb612f0d86726047e23245c20ca72
Eling Meyatmaja <eling.atma@gmail.com>
2022-02-10 10:30:34 +0700

    category post page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\TagController;
 use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Post;
@@ -40,6 +42,8 @@ Route::get('/about', AboutController::class)->name('about');
 Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
+Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('categories.show');
+Route::get('/tags/{tag:slug}', [TagController::class, 'show'])->name('tags.show');
 
 
 Route::get('/dashboard', function () {
